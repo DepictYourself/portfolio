7c955aea576fed5879a7616f264cf176685daa7d
Agoston Toth <totha014@gmail.com>
2018-03-15 00:10:45 +0100

    Add feature section with svg webpack support

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
 const extractSass = new ExtractTextPlugin({
     filename: "[name].css",
@@ -16,7 +17,34 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
-            },         
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    { 
+                        loader: 'svg-sprite-loader',
+                        options: {
+                            extract: true,
+                            spriteFilename: 'sprite-[hash:6].svg'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [
+                    {
+                        loader: "css-loader",
+                        options: { minimize: true }
+                    },
+                    {
+                        loader: "sass-loader"
+                    }],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            },  
             {
                 test: /\.html$/,
                 use: [
@@ -27,27 +55,14 @@ module.exports = {
                 ]
             },
             {
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    use: [{
-                        loader: "css-loader",
-                        options: { minimize: true }
-                    }, {
-                        loader: "sass-loader"
-                    }],
-                    // use style-loader in development
-                    fallback: "style-loader"
-                })
-            },
-            {
                 test: /\.(png|jpg|gif)$/,
                 use: [
-                  {
-                    loader: "file-loader",
-                    options: {}  
-                  }
+                    {
+                        loader: "file-loader",
+                        options: {}  
+                    }
                 ]
-              }
+            }
             
         ]
     },
@@ -57,6 +72,7 @@ module.exports = {
             template: "./src/index.html",
             filename: "./index.html"
         }),
-        new CleanWebpackPlugin(['dist'])
+        new CleanWebpackPlugin(['dist']),
+        new SpriteLoaderPlugin()
     ]
 }
