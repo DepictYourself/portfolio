af81b386eb7382dd57e2a4d91c968e31f6461e56
Agoston Toth <totha014@gmail.com>
2018-03-14 10:57:30 +0100

    Install Clean webpack plugin. So the dist folder is clearned before a new build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const extractSass = new ExtractTextPlugin({
     filename: "[name].[contenthash].css",
@@ -29,7 +30,8 @@ module.exports = {
                 test: /\.scss$/,
                 use: extractSass.extract({
                     use: [{
-                        loader: "css-loader"
+                        loader: "css-loader",
+                        options: { minimize: true }
                     }, {
                         loader: "sass-loader"
                     }],
@@ -46,5 +48,6 @@ module.exports = {
             template: "./src/index.html",
             filename: "./index.html"
         }),
+        new CleanWebpackPlugin(['dist'])
     ]
 }
