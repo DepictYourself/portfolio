e2627bf8e5f47a93dca9a32068ceb42575f8da1e
Agoston Toth <totha014@gmail.com>
2018-03-16 18:51:28 +0100

    Remove leftover test line from index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ import css from './styles.scss';
 const arr = [1, 2, 3];
 const iAmJavascriptES6 = () => console.log(...arr);
 window.iAmJavascriptES6 = iAmJavascriptES6;
-console.log("what the fu");
