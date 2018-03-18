5df05e1479898bbea302c5700d4321610bf530a9
Agoston Toth <totha014@gmail.com>
2018-03-18 18:33:43 +0100

    Add media queries to HEADER section

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ module.exports = {
                 use: [
                     {
                         loader: "html-loader",
-                        options: { minimize: true}
+                        options: { minimize: true }
                     }
                 ]
             },
